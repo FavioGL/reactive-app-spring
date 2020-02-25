da807c444ca5777a55b24a08a3b963ff793c5945
Favio <hugofavio@gmail.com>
2020-02-24 22:29:53 -0800

    functional ws

--- src/main/java/com/breakbot/reactive/document/Item.java
+++ src/main/java/com/breakbot/reactive/document/Item.java
@@ -6,12 +6,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document //like @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item {
-    @Id
     private String id;
     private String description;
     private Double price;
